0e4442219d0400b152445a6fa0363c03469d3293
Ashikur Rahman <ashikurrahman2765@gmail.com>
2024-03-25 15:30:49 +0600

    impliment jwt verify functionality

--- index.js
+++ index.js
@@ -25,6 +25,23 @@ const client = new MongoClient(uri, {
     }
 });
 
+const verifyJWT = (req, res, next) => {
+    
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(404).send({ error: true, message: 'Unauthorization Access' });
+    }
+    const token = authorization.split(' ')[1];
+    
+    jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) => {
+        if (error) {
+            return res.status(404).send({ error: true, message: 'Unauthorization Access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+};
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -37,34 +54,31 @@ async function run() {
         app.post('/jwt', (req, res) => {
             const user = req.body;
             const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
-            res.send({token});
+            res.send({ token });
         });
-        
+
         //Services route
         app.get('/services', async (req, res) => {
             // const cursor = serviceCollection.find();
             // const result = await cursor.toArray();
             const result = await serviceCollection.find().toArray();
             res.send(result);
-        })
+        });
 
         app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-
+            const query = { _id: new ObjectId(id) };
             const options = {
-
                 // Include only the `title` and `imdb` fields in each returned document
                 projection: { title: 1, price: 1, service_id: 1, img: 1 },
             };
 
-
             const result = await serviceCollection.findOne(query, options);
-            res.send(result)
+            res.send(result);
         });
 
         //Booking service
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings', verifyJWT, async (req, res) => {
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email }
