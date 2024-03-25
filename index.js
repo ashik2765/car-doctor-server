70c9eb7609629f63f8d15df328fa000f322aaf73
Ashikur Rahman <ashikurrahman2765@gmail.com>
2024-03-25 23:11:14 +0600

    add jwt

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ async function run() {
         //jwt route
         app.post('/jwt', (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: 5 });
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
             res.send({ token });
         });
 
